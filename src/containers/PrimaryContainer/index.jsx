596792f35fd3d91955264ac38a327cb9fc68d044
Henrybeecoder <omofonmwanhenrynosa@gmail.com>
2022-10-31 01:39:46 +0100

    pus

--- src/containers/PrimaryContainer/index.jsx
+++ src/containers/PrimaryContainer/index.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import styles from "./style.module.css"
+import React from "react";
+import styles from "./style.module.css";
+import logo from "../../assets/logo.svg";
+import droplet from "../../assets/svg/droplet.svg"
 
-export default function PrimaryContainer (props) {
-    return (
-        <div className={styles.container}>
-          <div className={styles.containerPart}>
-         {props.children}
+export default function PrimaryContainer(props) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerPart}>
+        <div className={styles.logoContainer}>
+          <img src={logo} alt="" />
         </div>
-        
+        {props.children}
+      </div>
+      <div className={styles.dropletFlex}>
+            <img src={droplet} alt=""/>
+            <img src={droplet} alt=""/>
+            <img src={droplet} alt=""/>
         </div>
-    )
+        <div className={styles.footer}>
+            <p>&#169;2022 iFuel. All rights reserved.</p>
+        </div>
+    </div>
+  );
 }
--- src/screens/StartPage/index.jsx
+++ src/screens/StartPage/index.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import PrimaryContainer from '../../containers/PrimaryContainer'
+import styles from "./style.module.css"
+import customer from "../../assets/svg/customer.svg"
+import vendor from "../../assets/svg/vendor.svg"
 
-export default function StartPage () {
+export default function StartPage() {
     return (
         <PrimaryContainer>
-            <h1>start</h1>
+            <div className={styles.startContainer}>
+                <h1>Which user type are you?</h1>
+                <p className={styles.select}>Please select as adequate</p>
+                <div className={styles.pointerFlex}>
+                    <div className={styles.container}>
+                        <img src={customer} alt="" />
+                        <p>Customer</p>
+                    </div>
+                    <div className={styles.container}>
+                        <img src={vendor} alt="" />
+                        <p>Vendor</p>
+                    </div>
+                </div>
+            </div>
         </PrimaryContainer>
-         
-        
+
+
     )
 }
